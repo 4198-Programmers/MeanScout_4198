cb49b888a0ead0fe73458fa7f173f72e02c6f674
Aiden Lee <aidenjlee9478@gmail.com>
2022-10-13 21:13:02 -0500

    This might work instead

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ function postSurvey(surveyJson){
   xhr.open("POST", "http://data.team4198.org:8000/scouting");
 
   xhr.setRequestHeader("Accept", "text/plain");
-  xhr.setRequestHeader("Content-Type", "text/plain");
+  xhr.setRequestHeader("Content-Type", "application/json");
 
   xhr.onload = function () {
 
